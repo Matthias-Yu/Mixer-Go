2ad8e54df6782f1df00f1800016dc1d53a10503e
Matthias <Matthias.Yu@outlook.com>
2024-02-11 11:09:51 +0000

    update the mod

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
 
 func main() {
 	app := fiber.New()
 
+	userSample := User{
+		Name: "John",
+		Age:  25,
+	}
+
+	userSampleStr := fmt.Sprintf("%v", userSample)
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString(userSampleStr)
 	})
+
 
 	err := app.Listen(":3000")
 	if err != nil {
